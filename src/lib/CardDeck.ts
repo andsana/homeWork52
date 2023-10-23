b32e273a1a4570c7221ad5c3e7b4d27c9a7f9667
Susanna Andreeva <sanadex90@gmail.com>
2023-10-23 21:28:28 +0900

    correct PokerHand.ts

--- src/lib/CardDeck.ts
+++ src/lib/CardDeck.ts
@@ -4,27 +4,27 @@ const suits = ['hearts', 'diams', 'clubs', 'spades'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 export default class CardDeck {
-    private deck: Card[];
+  private deck: Card[];
 
-    constructor() {
-        this.deck = [];
-        for (const suit of suits) {
-            for (const rank of ranks) {
-                this.deck.push(new Card(rank, suit));
-            }
-        }
+  constructor() {
+    this.deck = [];
+    for (const suit of suits) {
+      for (const rank of ranks) {
+        this.deck.push(new Card(rank, suit));
+      }
     }
+  }
 
-    getCard(): Card {
-        const randomIn = Math.floor(Math.random() * this.deck.length);
-        return this.deck.splice(randomIn, 1)[0];
-    }
+  getCard(): Card {
+    const randomIn = Math.floor(Math.random() * this.deck.length);
+    return this.deck.splice(randomIn, 1)[0];
+  }
 
-    getCards(howMany: number): Card[] {
-        const cards: Card[] = [];
-        for (let i = 0; i < howMany; i++) {
-            cards.push(this.getCard());
-        }
-        return cards;
+  getCards(howMany: number): Card[] {
+    const cards: Card[] = [];
+    for (let i = 0; i < howMany; i++) {
+      cards.push(this.getCard());
     }
+    return cards;
+  }
 }
--- src/lib/PokerHand.ts
+++ src/lib/PokerHand.ts
@@ -1,47 +1,58 @@
-import Card from "./Card";
+import Card from './Card';
 
 export default class PokerHand {
-    private hand: Card[];
+  private hand: Card[];
 
-    constructor(hand: Card[]) {
-        this.hand = hand;
+  constructor(hand: Card[]) {
+    this.hand = hand;
+  }
+
+  getOutcome(): string {
+    if (this.isPair()) {
+      return 'Одна пара';
+    } else if (this.isTwoPairs()) {
+      return 'Две пары';
+    } else if (this.isThreeOfAKind()) {
+      return 'Тройка';
+    } else if (this.isFlush()) {
+      return 'Флэш';
+    } else {
+      return 'Старшая карта';
     }
+  }
 
-    getOutcome(): string {
-        if (this.isPair()) {
-            return "Одна пара";
-        } else if (this.isTwoPairs()) {
-            return "Две пары";
-        } else if (this.isThreeOfAKind()) {
-            return "Тройка";
-        } else if (this.isFlush()) {
-            return "Флэш";
-        } else {
-            return "Старшая карта";
+  private isPair(): boolean {
+    const ranks = this.hand.map(card => card.rank);
+    const uniqueRanks = new Set(ranks);
+    return uniqueRanks.size === 4;
+  }
+
+  private isThreeOfAKind(): boolean {
+    const ranks = this.hand.map(card => card.rank);
+    for (let i = 0; i < ranks.length; i++) {
+      let count = 0;
+      for (let j = 0; j < ranks.length; j++) {
+        if (ranks[i] === ranks[j]) {
+          count++;
         }
+      }
+      if (count === 3) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    private isPair(): boolean {
-        const ranks = this.hand.map(card => card.rank);
-        const uniqueRanks = new Set(ranks);
-        return uniqueRanks.size === 3;
-    }
+  private isTwoPairs(): boolean {
+    const ranks = this.hand.map(card => card.rank);
+    const uniqueRanks = new Set(ranks);
+    return uniqueRanks.size === 3;
+  }
 
-    private isTwoPairs(): boolean {
-        const ranks = this.hand.map(card => card.rank);
-        const uniqueRanks = new Set(ranks);
-        return uniqueRanks.size === 1;
-    }
-
-    private isThreeOfAKind(): boolean {
-        const ranks = this.hand.map(card => card.rank);
-        const uniqueRanks = new Set(ranks);
-        return uniqueRanks.size === 2;
-    }
-
-    private isFlush(): boolean {
-        const ranks = this.hand.map(card => card.rank);
-        const uniqueRanks = new Set(ranks);
-        return uniqueRanks.size === 0;
-    }
+  private isFlush(): boolean {
+    const ranks = this.hand.map(card => card.rank);
+    const uniqueRanks = new Set(ranks);
+    return uniqueRanks.size === 1;
+  }
 }
+
