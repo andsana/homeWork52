a58b59223404266048b82154f931cb2479d5a344
Susanna Andreeva <sanadex90@gmail.com>
2023-10-23 17:15:03 +0900

    Add Class PokerHand

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Card from './lib/Card';
 import CardDeck from './lib/CardDeck';
+import PokerHand from "./lib/PokerHand";
 import './App.css';
 import MyCard from './MyCard';
 
 const App = () => {
     const [cards, setCards] = useState<Card[]>([]);
+    const [outcome, setOutcome] = useState<string>('');
 
     const dealCards = () => {
         const cardDeck = new CardDeck();
         const drawnCards = cardDeck.getCards(5);
         setCards(drawnCards);
+
+        const pokerHand = new PokerHand(drawnCards);
+        const handOutcome = pokerHand.getOutcome();
+        setOutcome(handOutcome);
     };
 
     return (
         <div className="App">
-            <button className="button" onClick={dealCards}>Раздать карты</button>
+            <button className="button" onClick={dealCards}>Deal draw</button>
             {cards.length !== 0 && (
                 <div className="playingCards faceImages">
                     {cards.map((card, index) => (
-                        <MyCard key={index} rank={card.rank} suit={card.suit} />
+                        <MyCard key={index} rank={card.rank} suit={card.suit}/>
                     ))}
                 </div>
             )}
+            {outcome && <div>{outcome}</div>}
         </div>
     );
 };
